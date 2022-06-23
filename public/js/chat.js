80ab16a76dcda80b3f41a2c4527c9c69e7c2ce8b
Maruf Bangabashi <alenmaruf.am@gmail.com>
2022-06-23 15:17:55 +0600

    Form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,25 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageFormButton = $messageForm.querySelector('button')
+const $messageFormInput =$messageForm.querySelector('input')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('message',(message)=>{
     console.log(message)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(e)=>{
+$messageForm.addEventListener('submit',(e)=>{
    e.preventDefault()
+
+   $messageFormButton.setAttribute('disabled','disabled')
 
    const message = e.target.elements.message.value
 
    socket.emit('sendMessage',message,(error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value =''
+        $messageFormInput.focus()
         if(error){
             return console.log(error)
         }
@@ -18,16 +28,20 @@ document.querySelector('#message-form').addEventListener('submit',(e)=>{
       
 })
 
-document.querySelector('#send-location').addEventListener('click',()=>{
+$sendLocationButton.addEventListener('click',()=>{
+
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser!')
     }
+
+    $sendLocationButton.setAttribute('disabled','disabled')
 
     navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendLocation',{
             latitude : position.coords.latitude,
             longitude: position.coords.longitude
         },(error)=>{
+            $sendLocationButton.removeAttribute('disabled')
             if(error){
                 return console.log(error)
             }
